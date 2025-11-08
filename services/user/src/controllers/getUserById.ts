04fc948214098e0a0324480e025d9e9a6c3566e0
coder7475 <robiulhossain7475@gmail.com>
2025-11-08 11:30:17 +0600

    feat(user): updated getUserById controller for dynamic id

--- services/user/src/controllers/getUserById.ts
+++ services/user/src/controllers/getUserById.ts
@@ -1,13 +1,24 @@
+import { User } from "@/generated/prisma/client";
 import prisma from "@/prisma_db";
 import { Request, Response, NextFunction } from "express";
 
+// users/:id?field=id|authUserId
 const getUserById = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = req.params;
 
-    const user = await prisma.user.findUnique({
-      where: { id },
-    });
+    const field = req.query.field as string;
+
+    let user: User;
+    if (field === "authUserId") {
+      user = await prisma.user.findUnique({
+        where: { authUserId: id },
+      });
+    } else {
+      user = await prisma.user.findUnique({
+        where: { id },
+      });
+    }
 
     if (!user) {
       return res.status(404).json({
@@ -22,3 +33,5 @@ const getUserById = async (req: Request, res: Response, next: NextFunction) => {
     next(error);
   }
 };
+
+export default getUserById;
