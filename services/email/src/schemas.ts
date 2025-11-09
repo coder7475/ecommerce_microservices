c90616e4bdf36b2f2dd2b94bf7da39bbb17180a3
coder7475 <robiulhossain7475@gmail.com>
2025-11-09 12:33:12 +0600

    feat(auth): create schemas and do migration

--- services/email/src/schemas.ts
+++ services/email/src/schemas.ts
@@ -1,1 +1,9 @@
 import { z } from "zod";
+
+export const emailCreateSchema = z.object({
+  recipient: z.email(),
+  subject: z.string().min(1, "Subject is required"),
+  body: z.string().min(1, "Body is required"),
+  source: z.string(),
+  sender: z.email().optional(),
+});
