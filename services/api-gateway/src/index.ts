43643fc66be9601cb2d7c5f8b26be7735f811f1a
coder7475 <robiulhossain7475@gmail.com>
2025-11-11 13:02:07 +0600

    build(api-gateway): fix code and dockerfile for api gateway

--- services/api-gateway/src/index.ts
+++ services/api-gateway/src/index.ts
@@ -89,7 +89,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 
 // Start server (for local development)
 const PORT = process.env.PORT || 3000;
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV) {
   app.listen(PORT, () => {
     console.log(`Server running on port http://localhost:${PORT}/api`);
   });
