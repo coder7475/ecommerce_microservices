0a30031a333ceba42a6c41aaa7a37743eb3611e8
coder7475 <robiulhossain7475@gmail.com>
2025-11-12 11:16:52 +0600

    feat(api-gateway): Update auth middleware to verify token

--- services/api-gateway/src/middlewares.ts
+++ services/api-gateway/src/middlewares.ts
@@ -1,11 +1,27 @@
 import { Request, Response, NextFunction } from "express";
 import { FunctionMap } from "./types";
+import axios from "axios";
 
-const auth = (req: Request, res: Response, next: NextFunction) => {
+const auth = async (req: Request, res: Response, next: NextFunction) => {
   if (!req.headers["authorization"]) {
     return res.status(401).json({ error: "Unauthorized" });
   }
-  next();
+  try {
+    const token = req.headers["authorization"]?.split(" ")[1];
+
+    const { data } = await axios.post("/auth/verify-token", {
+      accessToken: token,
+    });
+
+    req.headers["x-user-id"] = data.user.id;
+    req.headers["x-user-email"] = data.user.email;
+    req.headers["x-user-name"] = data.user.name;
+
+    next();
+  } catch (error) {
+    console.log("[Auth Middleware]", error);
+    return res.status(401).json({ error: "Unauthorized" });
+  }
 };
 
 const middlewares: FunctionMap = { auth };
