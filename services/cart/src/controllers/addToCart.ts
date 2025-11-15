aa214df976b0422c800a1e4708bddafe519411de
coder7475 <robiulhossain7475@gmail.com>
2025-11-15 11:03:17 +0600

    feat(cart): create add to cart route and controller signature

---
+++ services/cart/src/controllers/addToCart.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from "express";
+
+const addToCartController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // Implementation for adding item to cart
+};
+
+export default addToCartController;
