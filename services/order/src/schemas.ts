69a3d75ff3084e224e2241fd7f973a43c4b8302d
coder7475 <robiulhossain7475@gmail.com>
2025-11-17 13:01:30 +0600

    feat(order): create zod schemas

--- services/order/src/schemas.ts
+++ services/order/src/schemas.ts
@@ -1,2 +1,15 @@
+import { userInfo } from "os";
 import { z } from "zod";
 
+export const OrderSchema = z.object({
+  userId: z.string(),
+  userName: z.string(),
+  userEmail: z.email(),
+  cartSessionId: z.string(),
+});
+
+export const CartItemSchema = z.object({
+  productId: z.string().min(1),
+  inventoryId: z.string().min(1),
+  quantity: z.number().min(1),
+})
