c2412da3b2b097e274930eb9147f51aa05c89214
coder7475 <robiulhossain7475@gmail.com>
2025-11-24 14:46:45 +0600

    feat: finish the receiver from  cart and email service

--- services/cart/src/receiver.ts
+++ services/cart/src/receiver.ts
@@ -1,5 +1,6 @@
 import amqlib from 'amqplib';
 import { QUEUE_URL } from './config';
+import redisClient from './redis_client';
 
 const receiveFromQueue = async (queueName: string, callback: (message: string) => void) => {
     const connection = await amqlib.connect(QUEUE_URL);
@@ -23,4 +24,12 @@ const receiveFromQueue = async (queueName: string, callback: (message: string) =
 
 receiveFromQueue('clear-cart', msg => {
     console.log(`Received message: ${msg}`);
+    const parsedMessage = JSON.parse(msg);
+
+    const cartSessionId = parsedMessage.cartSessionId;
+
+    redisClient.del(`session:${cartSessionId}`);
+    redisClient.del(`cart:${cartSessionId}`)
+
+    console.log('cart cleared');
 })
--- services/email/src/receiver.ts
+++ services/email/src/receiver.ts
@@ -1,5 +1,6 @@
 import amqlib from 'amqplib';
-import { QUEUE_URL } from './config';
+import { DEFAULT_SENDER_MAIL, QUEUE_URL, transporter } from './config';
+import prisma from './prisma_db';
 
 const receiveFromQueue = async (queueName: string, callback: (message: string) => void) => {
     const connection = await amqlib.connect(QUEUE_URL);
@@ -21,6 +22,38 @@ const receiveFromQueue = async (queueName: string, callback: (message: string) =
 
 }
 
-receiveFromQueue('send-email', msg => {
+receiveFromQueue('send-email', async (msg) => {
     console.log(`Received message: ${msg}`);
+
+    const parsedBody = JSON.parse(msg);
+    const { userEmail, grandTotal, id } = parsedBody;
+    const from = DEFAULT_SENDER_MAIL;
+    const subject = 'Order Confirmation';
+    const body = `Thank you for your order. Your order id is ${id}. Your order total is $${grandTotal}`;
+
+    const emailOptions = {
+        from,
+        to: userEmail,
+        subject,
+        text: body
+    }
+
+    // send the email
+    const { rejected } = await transporter.sendMail(emailOptions);
+    if (rejected.length) {
+        console.log("Email Rejected: ", rejected);
+        return;
+    }
+
+    await prisma.email.create({
+        data: {
+            sender: from,
+            recipient: userEmail,
+            subject,
+            body,
+            source: 'Order Confirmation'
+        }
+    })
+
+    console.log("Email Sent");
 })
